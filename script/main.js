f0b146d22acb02dae282e38fec8ef581f7ce8f6b
Andrea De Martino <ademartino1992@gmail.com>
2020-04-09 17:38:17 +0200

    Fix: Minor Fixing

--- script/main.js
+++ script/main.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {
-
+  
   var scrollLeft = $('.left-icon');
   var scrollRight = $('.right-icon');
   var logo = $('.logo');
   var logoLen = 1;
   var active = $('.item-active');
 
-
+  // Log img Attiva
   console.log('Immagine attiva: ' + logoLen +'.jpg');
 
-  // Scorrimento a sinistra
-
+  // Scorrimento a sinistra con click
   scrollLeft.click(function () {
     active = $('.item-active');
-    
     // Doppio controllo, su contatore immagini e su classe first
     if ( logoLen > 1 && !active.hasClass('item-first') ){
       logoLen -- ;
@@ -27,49 +25,40 @@ $(document).ready(function () {
     }
   })
 
-    // Scorrimento a destra
-    scrollRight.click(function () { 
+  // Scorrimento a destra con click
+  scrollRight.click(function () { 
     active = $('.item-active');
-
     // Doppio controllo, su contatore e su classe last
     if ( logoLen < 4 && !active.hasClass('item-last') ){
-
       logoLen ++ ;
       console.log('Immagine attiva: ' + logoLen +'.jpg');
       logo.fadeOut(500,function(){
         logo.attr('src','img/' + logoLen + '.jpg')
         logo.fadeIn(500)
       })
-      
       active.next().addClass('item-active');
       active.first().removeClass('item-active');
     }
   })
 
-  // Gestione tasti
-  
+  // Scorrimento a destra e sinistra con tastiera
   $(document).keydown( function(e){
     var key = (e.keyCode);
     
     if (key == 39) {
       active = $('.item-active');
-      
-      // Doppio controllo, su contatore e su classe last
       if ( logoLen < 4 && !active.hasClass('item-last') ){
-        
         logoLen ++ ;
         console.log('Immagine attiva: ' + logoLen +'.jpg');
         logo.fadeOut(500,function(){
           logo.attr('src','img/' + logoLen + '.jpg')
           logo.fadeIn(500)
         })
-        
         active.next().addClass('item-active');
         active.first().removeClass('item-active');
       }
     } else if (key == 37) {
         active = $('.item-active');
-        // Doppio controllo, su contatore immagini e su classe first
         if ( logoLen > 1 && !active.hasClass('item-first') ){
           logoLen -- ;
           console.log('Immagine attiva: ' + logoLen +'.jpg');    
