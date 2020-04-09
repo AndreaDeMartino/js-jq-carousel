7b3fcea14bea2252fc2009bd73fcfc3f0de8c66a
Andrea De Martino <ademartino1992@gmail.com>
2020-04-09 17:29:42 +0200

    Creation: Add Js Keyboard

--- script/main.js
+++ script/main.js
@@ -6,12 +6,14 @@ $(document).ready(function () {
   var logoLen = 1;
   var active = $('.item-active');
 
+
   console.log('Immagine attiva: ' + logoLen +'.jpg');
 
   // Scorrimento a sinistra
+
   scrollLeft.click(function () {
     active = $('.item-active');
-
+    
     // Doppio controllo, su contatore immagini e su classe first
     if ( logoLen > 1 && !active.hasClass('item-first') ){
       logoLen -- ;
@@ -31,6 +33,7 @@ $(document).ready(function () {
 
     // Doppio controllo, su contatore e su classe last
     if ( logoLen < 4 && !active.hasClass('item-last') ){
+
       logoLen ++ ;
       console.log('Immagine attiva: ' + logoLen +'.jpg');
       logo.fadeOut(500,function(){
@@ -43,6 +46,45 @@ $(document).ready(function () {
     }
   })
 
+  // Gestione tasti
+  
+  $(document).keydown( function(e){
+    var key = (e.keyCode);
+    
+    if (key == 39) {
+      active = $('.item-active');
+      
+      // Doppio controllo, su contatore e su classe last
+      if ( logoLen < 4 && !active.hasClass('item-last') ){
+        
+        logoLen ++ ;
+        console.log('Immagine attiva: ' + logoLen +'.jpg');
+        logo.fadeOut(500,function(){
+          logo.attr('src','img/' + logoLen + '.jpg')
+          logo.fadeIn(500)
+        })
+        
+        active.next().addClass('item-active');
+        active.first().removeClass('item-active');
+      }
+    } else if (key == 37) {
+        active = $('.item-active');
+        // Doppio controllo, su contatore immagini e su classe first
+        if ( logoLen > 1 && !active.hasClass('item-first') ){
+          logoLen -- ;
+          console.log('Immagine attiva: ' + logoLen +'.jpg');    
+          logo.fadeOut(500,function(){
+            logo.attr('src','img/' + logoLen + '.jpg')
+            logo.fadeIn(500)
+          })
+          active.prev().addClass('item-active');
+          active.last().removeClass('item-active');
+        }
+      }
+  })
+
+
+  
 
   
 
