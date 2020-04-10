3cebacb500c913154eac2104a3d5c6913a0e598f
Andrea De Martino <ademartino1992@gmail.com>
2020-04-10 13:05:04 +0200

    Creation: Added clickable items to change img and improved code

--- script/main.js
+++ script/main.js
@@ -1,87 +1,109 @@
 $(document).ready(function () {
-  
+  // General Variables
   var scrollLeft = $('.left-icon');
   var scrollRight = $('.right-icon');
   var logo = $('.logo');
-  var logoLen = 1;
+  var logoCont = 1;
   var active = $('.item-active');
 
-  // Log img Attiva
-  console.log('Immagine attiva: ' + logoLen +'.jpg');
+  // Variables for Items
+  var items = $('.selector a');
+  var itemOne = $('.item-first');
+  var itemTwo = itemOne.next();
+  var itemThree = itemTwo.next();
+  var itemFour = $('.item-last');
 
-  // Scorrimento a sinistra con click
+  // Case for items click
+  switch (items) {
+    case itemOne.click(function () {
+      logoCont = 1;
+      items.removeClass('item-active');
+      itemOne.addClass('item-active');
+      changeImg(logoCont);
+    }):
+      break;
+  
+    case itemTwo.click(function () {
+      logoCont = 2;
+      items.removeClass('item-active');
+      itemTwo.addClass('item-active');
+      changeImg(logoCont);
+    }):
+      break;
+
+    case itemThree.click(function () {
+      logoCont = 3;
+      items.removeClass('item-active');
+      itemThree.addClass('item-active');
+      changeImg(logoCont);
+    }):
+      break;
+
+    case itemFour.click(function () {
+      logoCont = 4;
+      items.removeClass('item-active');
+      itemFour.addClass('item-active');
+      changeImg(logoCont);
+    }):
+      break;
+  }
+
+  // Left scroll with click
   scrollLeft.click(function () {
     active = $('.item-active');
-    // Doppio controllo, su contatore immagini e su classe first
-    if ( logoLen > 1 && !active.hasClass('item-first') ){
-      logoLen -- ;
-      console.log('Immagine attiva: ' + logoLen +'.jpg');    
-      logo.fadeOut(500,function(){
-        logo.attr('src','img/' + logoLen + '.jpg')
-        logo.fadeIn(500)
-      })
+
+    if ( logoCont > 1 && !active.hasClass('item-first') ){
+      logoCont -- ;
+      changeImg(logoCont);
       active.prev().addClass('item-active');
       active.last().removeClass('item-active');
     }
   })
 
-  // Scorrimento a destra con click
+  // Right scroll with click
   scrollRight.click(function () { 
     active = $('.item-active');
-    // Doppio controllo, su contatore e su classe last
-    if ( logoLen < 4 && !active.hasClass('item-last') ){
-      logoLen ++ ;
-      console.log('Immagine attiva: ' + logoLen +'.jpg');
-      logo.fadeOut(500,function(){
-        logo.attr('src','img/' + logoLen + '.jpg')
-        logo.fadeIn(500)
-      })
+
+    if ( logoCont < 4 && !active.hasClass('item-last') ){
+      logoCont ++ ;
+      changeImg(logoCont);
       active.next().addClass('item-active');
       active.first().removeClass('item-active');
     }
   })
 
-  // Scorrimento a destra e sinistra con tastiera
+  // Scroll with Keyboard
   $(document).keydown( function(e){
     var key = (e.keyCode);
     
+    // Right arrow
     if (key == 39) {
       active = $('.item-active');
-      if ( logoLen < 4 && !active.hasClass('item-last') ){
-        logoLen ++ ;
-        console.log('Immagine attiva: ' + logoLen +'.jpg');
-        logo.fadeOut(500,function(){
-          logo.attr('src','img/' + logoLen + '.jpg')
-          logo.fadeIn(500)
-        })
+      if ( logoCont < 4 && !active.hasClass('item-last') ){
+        logoCont ++ ;
+        changeImg(logoCont);
         active.next().addClass('item-active');
         active.first().removeClass('item-active');
-      }
-    } else if (key == 37) {
+        }
+    }
+      // Left arrow
+      else if (key == 37) {
         active = $('.item-active');
-        if ( logoLen > 1 && !active.hasClass('item-first') ){
-          logoLen -- ;
-          console.log('Immagine attiva: ' + logoLen +'.jpg');    
-          logo.fadeOut(500,function(){
-            logo.attr('src','img/' + logoLen + '.jpg')
-            logo.fadeIn(500)
-          })
+        if ( logoCont > 1 && !active.hasClass('item-first') ){
+          logoCont -- ;  
+          changeImg(logoCont);
           active.prev().addClass('item-active');
           active.last().removeClass('item-active');
         }
       }
   })
 
-
-  
-
-  
-
-
-
-
-
-
-  
+  // Function for change img
+  function changeImg (cont){
+    logo.fadeOut(500,function(){
+      logo.attr('src','img/' + cont + '.jpg')
+      logo.fadeIn(500)
+    })
+  }
 
 });
