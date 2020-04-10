5b727fec2a3d5226d930cbc0a444a18fdd5d587f
Andrea De Martino <ademartino1992@gmail.com>
2020-04-10 14:32:22 +0200

    Revolution!: Created dinamic carousel, you can choose how much img u whant

--- script/main.js
+++ script/main.js
@@ -6,57 +6,47 @@ $(document).ready(function () {
   var logoCont = 1;
   var active = $('.item-active');
 
-  // Variables for Items
-  var items = $('.selector a');
-  var itemOne = $('.item-first');
-  var itemTwo = itemOne.next();
-  var itemThree = itemTwo.next();
-  var itemFour = $('.item-last');
+  //  Creation items and img
+  var selector = $('.selector');
+  var imgLen = parseInt(prompt('Inserisci numero img')); 
 
-  // Case for items click
-  switch (items) {
-    case itemOne.click(function () {
-      logoCont = 1;
-      items.removeClass('item-active');
-      itemOne.addClass('item-active');
-      changeImg(logoCont);
-    }):
-      break;
-  
-    case itemTwo.click(function () {
-      logoCont = 2;
-      items.removeClass('item-active');
-      itemTwo.addClass('item-active');
-      changeImg(logoCont);
-    }):
-      break;
-
-    case itemThree.click(function () {
-      logoCont = 3;
-      items.removeClass('item-active');
-      itemThree.addClass('item-active');
-      changeImg(logoCont);
-    }):
-      break;
-
-    case itemFour.click(function () {
-      logoCont = 4;
-      items.removeClass('item-active');
-      itemFour.addClass('item-active');
-      changeImg(logoCont);
-    }):
-      break;
+  for (var y = 1; y <= imgLen; y++){
+    if (y == 1){
+      selector.html('<a href="#" class= "item-first item-active'+ ' ' + y + '"' + '> <i class="fas fa-circle"></i></a>');
+    } else if (y == imgLen){
+      selector.append('<a href="#" class="item-last'+ ' ' + y + '"' + '><i class="fas fa-circle"></i></a>')
+    } else {
+      selector.append('<a class="'+ y + '"' + 'href="#"><i class="fas fa-circle"></i></a>');
+    }
   }
+
+  var items = $('.selector a');
+
+  items.click( function (){
+    for (x = 1; x <= imgLen; x++){
+      var itemClick = $(this);
+      if (itemClick.hasClass(x)){
+        console.log('cliccato pallino n.', x);
+        items.removeClass('item-active');
+        itemClick.addClass('item-active');
+        changeImg(x);
+        logoCont = x;
+      }
+    }
+    
+  })
 
   // Left scroll with click
   scrollLeft.click(function () {
     active = $('.item-active');
 
     if ( logoCont > 1 && !active.hasClass('item-first') ){
+      console.log('entrato sinistra click');
       logoCont -- ;
       changeImg(logoCont);
       active.prev().addClass('item-active');
       active.last().removeClass('item-active');
+      console.log('Img selezionata',logoCont);
     }
   })
 
@@ -64,11 +54,14 @@ $(document).ready(function () {
   scrollRight.click(function () { 
     active = $('.item-active');
 
-    if ( logoCont < 4 && !active.hasClass('item-last') ){
+    if ( logoCont < imgLen && !active.hasClass('item-last') ){
+      console.log('entrato destra click');
       logoCont ++ ;
       changeImg(logoCont);
       active.next().addClass('item-active');
       active.first().removeClass('item-active');
+      console.log('Img selezionata',logoCont);
+      
     }
   })
 
@@ -79,21 +72,27 @@ $(document).ready(function () {
     // Right arrow
     if (key == 39) {
       active = $('.item-active');
-      if ( logoCont < 4 && !active.hasClass('item-last') ){
+      if ( logoCont < imgLen && !active.hasClass('item-last') ){
+        console.log('entrato aumento tastiera');
         logoCont ++ ;
         changeImg(logoCont);
         active.next().addClass('item-active');
         active.first().removeClass('item-active');
+        console.log('Img selezionata',logoCont);
+        
         }
     }
       // Left arrow
       else if (key == 37) {
         active = $('.item-active');
         if ( logoCont > 1 && !active.hasClass('item-first') ){
+          console.log('entrato decremento tastiera');
+          
           logoCont -- ;  
           changeImg(logoCont);
           active.prev().addClass('item-active');
           active.last().removeClass('item-active');
+          console.log('Img selezionata',logoCont);
         }
       }
   })
@@ -101,7 +100,7 @@ $(document).ready(function () {
   // Function for change img
   function changeImg (cont){
     logo.fadeOut(500,function(){
-      logo.attr('src','img/' + cont + '.jpg')
+      logo.attr('src','https://i.pravatar.cc/400?img=' + cont)
       logo.fadeIn(500)
     })
   }
